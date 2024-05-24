2eb1873824c0ac2e24cc7b211623632c283f0a61
ipxkingytvip <155648055+ipxkingytvip@users.noreply.github.com>
2024-05-25 02:19:06 +0530

    Update m.py

--- m.py
+++ m.py
@@ -235,8 +235,8 @@ def handle_bgmi(message):
             target = command[1]
             port = int(command[2])  # Convert time to integer
             time = int(command[3])  # Convert port to integer
-            if time > 181:
-                response = "Error: Time interval must be less than 80."
+            if time > 311:
+                response = "Error: Time interval must be less than 310."
             else:
                 record_command_logs(user_id, '/bgmi', target, port, time)
                 log_command(user_id, target, port, time)
